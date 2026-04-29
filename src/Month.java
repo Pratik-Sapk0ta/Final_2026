79e742f5c1a97ad4753dea295b964eb6a323e98c
Pratik Sapkota <psapkot06@gmail.com>
2026-04-28 23:01:02 -0500

    save work

--- src/Month.java
+++ src/Month.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Month {
+    private ArrayList<Integer> dayTemp;
+
+    public Month();
+
+    daysTemp =new ArrayList<>();
+}
+
+    public void addDay(int temp) {
+
+    }
+
+    p
+
+
 }
