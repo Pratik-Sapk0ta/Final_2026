a7d7aa12654d8d3da54e466d480fbebdc4603013
Pratik Sapkota <psapkot06@gmail.com>
2026-05-02 11:34:46 -0500

    Don't know what happened

---
+++ src/viewer/WeatherGui.java
@@ -0,0 +1,19 @@
+package viewer;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class WeatherGui extends JFrame implements ActionListener{
+
+    private
+
+
+}
