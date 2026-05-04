2d5b319218977cd576b47b32592fd05abfc7651f
Pratik Sapkota <psapkot06@gmail.com>
2026-05-04 14:04:31 -0500

    Done with GUI

--- src/viewer/WeatherGui.java
+++ src/viewer/WeatherGui.java
@@ -18,8 +18,41 @@ public class WeatherGUI extends JFrame implements ActionListener{
     private JButton addButton;
     private JButton removeButton;
     private JButton calcButton;
-    private JButton toggleButton; //Switch from C to F
+    private JToggleButton toggleButton; //Switch from C to F
     private JButton resetButton;
+
+
+    //getters
+    public JButton getAddButton() {
+        return addButton;
+    }
+
+    public JButton getRemoveButton() {
+        return removeButton;
+    }
+
+    public JButton getCalcButton() {
+        return calcButton;
+    }
+
+    public JButton getToggleButton() {
+        return toggleButton;
+    }
+
+    public JButton getResetButton() {
+        return resetButton;
+    }
+
+    public String getInputText() {
+        return  inputField.getText();
+    }
+
+    public void clearInput() {
+        inputField.setText("");
+    }
+    public void setToggleButton(JButton toggleButton) {
+        this.toggleButton = toggleButton;
+    }
 
     public WeatherGUI() {
         super("Weather Calculator");
@@ -32,14 +65,33 @@ public class WeatherGUI extends JFrame implements ActionListener{
         toggleButton = new JButton("Switch Mood");
         resetButton = new JButton("Reset");
 
-        detLayout (new GridBagLayout());
+        setLayout (new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
+        gbc.gridx = 0; gbc.gridy = 0;
+        add( new JLable("Enter Value:"), gbc);
 
+        gbc.gridx = 1;
+        add(inputField, gbc);
 
+        gbc.gridx = 0;
+        gbc.gridx = 1;
+        add(addButton,gbc);
 
+        gbc.gridx = 1;
+        add(removeButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridx = 2;
+        add(calcButton, gbc);
+
+        gbc.gridx = 1;
+        add(toggleButton,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridx = 3;
+        gbc.gridwidth = 2;
+        add(resetButton, gbc);
     }
-
-
 }
